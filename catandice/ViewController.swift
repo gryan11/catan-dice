1d8ca6c8403b37b05a87383cea70f3cbfa504b8a
Gabriel Ryan <gabe@cs.columbia.edu>
2019-06-21 09:00:12 -0400

    baseline working

--- catandice/ViewController.swift
+++ catandice/ViewController.swift
@@ -9,12 +9,74 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var resetInd:Int = 30;
+    var rollInd:Int = 36;
+    
+    var rolls:[(Int, Int)] = [];
 
+    
+
+    
+//    required init?(coder aDecoder: NSCoder) {
+//        super.init();
+//        ResetRoll = 30;
+//        rollInd = 0;
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var Die1: UILabel!
+    
+    @IBOutlet weak var Die2: UILabel!
+    
+    @IBOutlet weak var Die3: UILabel!
+    
+    @IBAction func roll_button(_ sender: Any) {
+        
+        if (rollInd > resetInd) {
+        
+            rolls = resetRolls();
+            rollInd = 0;
+        }
+        
+        Die1.text = String(rolls[rollInd].0);
+        Die2.text = String(rolls[rollInd].1);
+        Die3.text = String(Int.random(in: 1...12));
+        
+        rollInd += 1;
+    }
+    
+    func shuffle(ap: [(Int, Int)]) -> [(Int, Int)] {
+        var a:[(Int, Int)] = ap;
+        var j:Int; var x:(Int, Int);
+        
+        for i in 0..<a.count {
+            j = Int.random(in: 0 ... i + 1);
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+        return a;
+    }
+    
+    func resetRolls () -> [(Int, Int)] {
+        var rolls:[(Int, Int)] = [];
+    
+//        for (var i=1; i<=6; i++) {
+        for i in 1...6 {
+//            for (var j=1; j<=6; j++) {
+            for j in 1...6 {
+                rolls.append((i, j));
+            }
+        }
+    
+        rolls = shuffle(ap: rolls);
+    
+        return rolls;
+    }
 }
 
